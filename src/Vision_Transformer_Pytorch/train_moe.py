0cd131a882c51e33df2cfbdd9ba4ec96d107111c
pmq_2 <phamminhquang289@gmail.com>
2025-06-12 04:00:45 -0500

    add warmup

--- src/Vision_Transformer_Pytorch/train_moe.py
+++ src/Vision_Transformer_Pytorch/train_moe.py
@@ -56,6 +56,7 @@ CUTMIX_ALPHA = 1.0
 CUTMIX_PROB = 0.5
 TEST_START_EPOCH = 50
 TEST_FREQUENCY = 2
+WARMUP_EPOCHS = 10
 
 # **************** DevOps Params ****************
 OUTPUT_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..', 'artifacts'))
@@ -297,7 +298,17 @@ def main():
     model = VisionTransformer(config).to(DEVICE)
     criterion = nn.CrossEntropyLoss()
     optimizer = optim.AdamW(model.parameters(), lr=LEARNING_RATE, weight_decay=0.05)
-    scheduler = optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=100)
+    #scheduler = optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=100)
+    T_max = 100
+    # if T_max = epoch. Pros: steady and predictable decay, improve convergence stability. Cons: complex models might not explore enough
+    # if T_max = 100. Pros: good for exploring, escape local minima
+    def lr_lambda(epoch):
+        if epoch < WARMUP_EPOCHS:
+            return (epoch + 1) / WARMUP_EPOCHS
+        else:
+            t = epoch - WARMUP_EPOCHS
+            return 0.5 * (1 + np.cos(np.pi * (t % T_max) / T_max))
+    scheduler = optim.lr_scheduler.LambdaLR(optimizer, lr_lambda)
 
     train_losses = []
     test_losses = []
